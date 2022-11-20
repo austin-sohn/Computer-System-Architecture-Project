5f1d276b172e30ec8e0e149f733eb80bf08d1c86
Austin Sohn <sohn.austin@gmail.com>
2022-11-19 22:06:29 -0800

    fixed white_circle

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # REQUIREMENTS
 
+- python 3.7.7+
 - pygame
   - python3 -m pip install -U pygame --user
+
+# HOW TO RUN
+
+- python3 main.py
+
+# CREATED BY
+
+- Austin Sohn
+-
--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import sys, pygame
-pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+def main():
+  pygame.init()
+  size = width, height = 320, 240
+  speed = [2, 2]
+  black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+  screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("white_circle.png")
-ballrect = ball.get_rect()
+  DEFAULT_IMAGE_SIZE = (50,50)
+  image = pygame.image.load("./images/white_circle.png")
+  ball = pygame.transform.scale(image, DEFAULT_IMAGE_SIZE)
+  ballrect = ball.get_rect()
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT: sys.exit()
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: sys.exit()
 
-  ballrect = ballrect.move(speed)
-  if ballrect.left < 0 or ballrect.right > width:
-    speed[0] = -speed[0]
-  if ballrect.top < 0 or ballrect.bottom > height:
-    speed[1] = -speed[1]
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+      speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+      speed[1] = -speed[1]
 
-  screen.fill(black)
-  screen.blit(ball, ballrect)
-  pygame.display.flip()
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+
+if __name__ == "__main__":
+  main()
